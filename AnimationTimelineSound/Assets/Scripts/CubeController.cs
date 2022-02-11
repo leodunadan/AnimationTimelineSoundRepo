2e1aeda39a4c44587319fc3907565c1cb3c1f25a
leodunadan <lea334@nyu.edu>
2022-02-11 12:11:22 +0000

    animation up until the robot where i got stuck with the animation

---
+++ AnimationTimelineSound/Assets/Scripts/CubeController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public Transform start;
+    public Transform end;
+
+    public float speed = 1.0f;
+
+    [Range(0,1)]
+    public float t = 0.0f; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = start.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+        t = Mathf.MoveTowards(t, 1.0f, speed * Time.deltaTime);
+        //Mathf.Lerp
+
+        transform.position = Vector3.Lerp(start.position, end.position, t);
+        transform.rotation = Quaternion.Slerp(start.rotation, end.rotation, t);
+    }
+}
